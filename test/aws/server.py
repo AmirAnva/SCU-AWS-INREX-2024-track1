4dfa8a7419414dab9df31ccd9dcde0184f63bef2
ImTimTong <130192910+ImTimTong@users.noreply.github.com>
2024-11-17 06:23:12 -0800

    Update server.py

--- test/aws/server.py
+++ test/aws/server.py
@@ -138,5 +138,34 @@ def favicon():
 def output(id):
     return Chat(id)
 
+# Example URL: localhost:3000/token
+@app.route("/token")
+def get_token_route():
+    token = get_token()
+    return jsonify({"token": token})
+
+
+# Example URL: localhost:3000/cameras?token=TOKEN&corner1=LAT|LONG&corner2=LAT|LONG
+@app.route("/cameras")
+def get_cameras_route():
+    token = request.args.get("token")
+    corner1 = request.args.get("corner1")
+    corner2 = request.args.get("corner2")
+    if not token or not corner1 or not corner2:
+        return jsonify({"error": "Token or geobox corners not found. Please provide."})
+    cameras = get_cameras_in_a_box(token, corner1, corner2)
+    return jsonify({"cameras": cameras})
+
+
+# Example URL: localhost:3000/camera-image?camera_id=CAMERA_ID&token=TOKEN
+@app.route("/camera-image")
+def get_camera_image_route():
+    camera_id = request.args.get("camera_id")
+    token = request.args.get("token")
+    if not camera_id or not token:
+        return jsonify({"error": "Camera ID or token not found. Please provide."})
+    image = get_camera_image(camera_id, token)
+    return image
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
